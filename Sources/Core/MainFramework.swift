be6fc750a399c7d3950c0688589061328d7da8c9
Muhittin Camdali <muhittincamdali@gmail.com>
2025-08-12 03:43:55 +0300

    chore(build): remove invalid tests and switch UIKit import to SwiftUI for visionOS (#8)

--- Sources/Core/MainFramework.swift
+++ Sources/Core/MainFramework.swift
@@ -1,5 +1,5 @@
 import Foundation
-import UIKit
+import SwiftUI
 
 /**
  * Main Framework Implementation
--- Tests/LinuxMain.swift
+++
@@ -1,7 +0,0 @@
-import XCTest
-
-import VisionOS-UI-FrameworkTests
-
-var tests = [XCTestCaseEntry]()
-tests += VisionOS-UI-FrameworkTests.allTests()
-XCTMain(tests)
